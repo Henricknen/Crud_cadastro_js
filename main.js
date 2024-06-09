085d60d6d956a672d7a79c70c3d45cd35ff09890
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-09 11:58:14 -0300

    implementando_funcao_que_envia_e_le_dados_para_o_'localstorage'_ultilizando_metodo_'setItem'_e_'getItem'

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "lhsf",
+    email: "lhsf@hotmail.com",
+    celular: "256326655",
+    cidade: "Martinopolis"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage() 
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+    
+}
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
